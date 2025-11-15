a65f8bd6bc43d237187803417206022589eb14e1
riya jatav <riyajatav86@.com>
2025-11-15 19:11:38 +0530

    code

---
+++ oopm.cpp/message.java
@@ -0,0 +1,7 @@
+message.cpp
+massage pasisng by CPP impliments msg passing by enabling us to control one object from another by passing massages or values.
+
+//create another variable int voume in tv class
+create a function that increasres the volume 
+assume that the initial volume is 0
+//create a method in remote class that can control the volume of tv.
